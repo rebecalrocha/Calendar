ef9dc423f5812fab0b2d2628fc5397d4befa98b9
rebecalrocha <rochalrebeca@gmail.com>
2020-05-01 11:29:08 -0300

    criado funcionalidades do sistema

---
+++ events-api/app/models.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password = db.Column(db.String(128))
+    events = db.relationship('Event', backref='owner', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.email)    
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(140))
+    start_time = db.Column(db.DateTime, index=True)
+    end_time = db.Column(db.DateTime, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Event {}>'.format(self.description)
+
+    def serialize(self):
+        return {
+            "description": self.description,
+            "start_time": self.start_time.strftime("%Y-%m-%dT%H:%M:%S"),
+            "end_time": self.end_time.strftime("%Y-%m-%dT%H:%M:%S"),
+        }
+
+        
--- events-api/app/routes.py
+++ events-api/app/routes.py
@@ -1,21 +1,72 @@
-from app import app
+#tipo controller, faz o mapeamento da rota com a funções
+from app import app, db
 from flask import request
 import json 
-
-#tipo controller, faz o mapeamento da rota com a funções
+from app.models import User, Event
+from datetime import datetime
 
 
 @app.route('/events', methods=['GET'])
 def get_events():
-    calendarEvents = {
-        "events": [
-            { "title": "event 1" },{ "title": "event 2" }
-        ]
-    }
-    return json.dumps(calendarEvents)
+    #usando ORM para fazer uma query 
+    #pegando objeto com id=1
+    logged_user = User.query.get(1)
 
-@app.route('/events', methods=['POST'])
+    #criando dicionario
+    events = {"events": []}
+
+    for event in logged_user.events:
+        events["events"].append(event.serialize())
+
+    #convertendo para json
+    return json.dumps(events)
+
+@app.route('/create-events', methods=['POST'])
 def create_event():
+    logged_user = User.query.get(1)
     event_params = json.loads(request.data)
-    print(event_params)
-    return "well done"
+    
+    event = Event()
+    event.description = event_params["description"]
+    event.start_time = datetime.strptime(event_params["start_time"], '%Y-%m-%dT%H:%M:%S')
+    event.end_time = datetime.strptime(event_params["end_time"], '%Y-%m-%dT%H:%M:%S')
+    event.user_id = logged_user.id
+
+    # salvando no banco
+    db.session.add(event)
+    db.session.commit()
+
+    #criando dicionario e convertendo para json
+    return json.dumps({"event": event.serialize()})
+
+@app.route('/delete-event', methods=['POST'])
+def delete_event():
+    logged_user = User.query.get(1)
+    event_params = json.loads(request.data)
+    print(event_params["event_id"])
+
+    event = Event.query.filter_by(id=event_params["event_id"], user_id=logged_user.id).first()
+    if event:
+        db.session.delete(event)
+        db.session.commit()
+        return "deleted"
+    return "event not found"
+
+@app.route('/edit-event', methods=['POST'])
+def edit_event():
+    logged_user = User.query.get(1)
+    event_params = json.loads(request.data)  
+
+    event = Event.query.filter_by(id=event_params["event_id"], user_id=logged_user.id).first()
+    if event:
+        if "description" in event_params:
+            event.description = event_params["description"]
+        if "start_time" in event_params:
+            event.start_time = datetime.strptime(event_params["start_time"], '%Y-%m-%dT%H:%M:%S')
+        if "end_time" in event_params:
+            event.end_time = datetime.strptime(event_params["end_time"], '%Y-%m-%dT%H:%M:%S')
+            print(event.end_time)
+        db.session.commit()
+    
+        return json.dumps({"event": event.serialize()})
+    return "event not found"
